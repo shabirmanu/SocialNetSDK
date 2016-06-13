24d470aace27a8485e9db0d4f266502e5f49a382
Shabir A <engr.shabir@yahoo.com>
2016-06-13 17:56:35 +0500

    changes to config

--- config/SocialNetSDK.php
+++ config/SocialNetSDK.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'fb_app_id' => env('FB_APP_ID'),
-    'fb_secret' => env('FB_APP_SECRET'),
+    'fb_app_id' => '137620776644369',
+    'fb_secret' => '5c19646c9b87aa8983222435c9e0f3c5',
 ];
